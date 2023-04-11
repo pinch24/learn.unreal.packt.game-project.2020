dd43624bebdc891e62411008926ce796c853a533
pinch24 <pinch24@gmail.com>
2023-04-11 14:25:05 +0900

    07. Utilities

---
+++ Dodgeball/Source/Dodgeball/DodgeballFunctionLibrary.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "DodgeballFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DODGEBALL_API UDodgeballFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+};
