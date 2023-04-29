077b3a516ea200c8060fa4d2b65ed3a521ee1d63
pinch24 <pinch24@gmail.com>
2023-04-29 23:05:40 +0900

    Chapter 11. Blend Space 1D, State Machine

---
+++ SuperSideScroller/Source/SuperSideScroller/SuperSideScroller_Player.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SuperSideScroller_Player.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+ASuperSideScroller_Player::ASuperSideScroller_Player()
+{
+	bIsSprinting = false;
+
+	GetCharacterMovement()->MaxWalkSpeed = 300.0f;
+}
+
+void ASuperSideScroller_Player::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("Sprint", IE_Pressed, this, &ASuperSideScroller_Player::Sprint);
+	PlayerInputComponent->BindAction("Sprint", IE_Released, this, &ASuperSideScroller_Player::StopSprinting);
+}
+
+void ASuperSideScroller_Player::Sprint()
+{
+	if (bIsSprinting == false) {
+		bIsSprinting = true;
+		GetCharacterMovement()->MaxWalkSpeed = 500.0f;
+	}
+}
+
+void ASuperSideScroller_Player::StopSprinting()
+{
+	if (bIsSprinting == true) {
+		bIsSprinting = false;
+		GetCharacterMovement()->MaxWalkSpeed = 300.0f;
+	}
+}
+
+void ASuperSideScroller_Player::ThrowProjectile()
+{
+	UE_LOG(LogTemp, Warning, TEXT("THROW PROJECTILE!"));
+}
+
