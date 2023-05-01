ed55848f2cb2d44ab5e8657ef90166aa4be4bf2b
pinch24 <pinch24@gmail.com>
2023-05-01 20:08:22 +0900

    Chapter 13. Enemy AI

---
+++ SuperSideScroller/Source/SuperSideScroller/PlayerProjectile.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerProjectile.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+APlayerProjectile::APlayerProjectile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	// Collision
+	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	CollisionComp->InitSphereRadius(15.0f);
+	CollisionComp->BodyInstance.SetCollisionProfileName("BlockAll");
+	CollisionComp->OnComponentHit.AddDynamic(this, &APlayerProjectile::OnHit);
+	RootComponent = CollisionComp;
+
+	// Movement
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileComp"));
+	ProjectileMovement->UpdatedComponent = CollisionComp;
+	ProjectileMovement->ProjectileGravityScale = 0.0f;
+	ProjectileMovement->InitialSpeed = 500.0f;
+	ProjectileMovement->MaxSpeed = 500.0f;
+
+	// Mesh
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepWorldTransform);
+
+	// Life
+	InitialLifeSpan = 3.0f;
+}
+
+void APlayerProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	UE_LOG(LogTemp, Warning, TEXT("HIT!!!!"));
+}
